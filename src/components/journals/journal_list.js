c9983e3f5dce72eae2d945dc356a00d22cc27b14
hugoclaude <hugocayeros@gmail.com>
2020-09-24 15:11:38 -0700

    bugs fixed, content added

--- src/components/journals/journal_list.js
+++ src/components/journals/journal_list.js
@@ -1,23 +1,43 @@
 import React, { Component, component } from 'react';
+import { JournalEntry } from './journal_entry';
 
 const rawJournalData = [
     {title: 'Post One', content: 'Post content', status: 'draft'},
-    {title: 'Post Two', content: 'Post content', status: 'draft'},
-    {title: 'Post Three', content: 'Post content', status: 'draft'},
-    {title: 'Post Four', content: 'Post content', status: 'draft'},
-]
+    {title: 'Post Two', content: 'Post content', status: 'Published'},
+    {title: 'Post Three', content: 'Post content', status: 'Published'},
+    {title: 'Post Four', content: 'Post content', status: 'published'},
+];
+
 // Class component
 export default class JournalList extends Component {
     constructor(props) {
         super();
 
         this.state = {
-            rawJournalData: rawJournalData,
+            journalData: rawJournalData,
             greeting: 'Hi there',
             isOpen: true
-        }
+        };
     }
+
     render() {
-        return <h2>{this.props.heading}</h2>
-    }
+        const journalEntries = this.state.journalData.map(journalEntry => {
+            return(
+                <div key={journalEntry.title}>
+                    <JournalEntry
+                        title={journalEntry.title}
+                        content={journalEntry.content}
+                        status={journalEntry.status}
+                    />
+                </div>
+            );
+        });
+
+    return(
+        <div>
+            <h1>{this.props.heading}</h1>
+            {journalEntries}
+        </div>
+    );   
+    };
 }
